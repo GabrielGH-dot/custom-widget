bb502de34323aa4bc7412dd1ccf167226893f3d4
GabrielGH-dot <gabriel.hauschildt@gmail.com>
2026-01-16 17:09:32 +0100

    build: release v1.0.5

--- gghtable.bundle.js
+++ gghtable.bundle.js
@@ -1,4 +1,4 @@
-(()=>{var E=`/* Contenedor de tabla */\r
+(()=>{var x=`/* Contenedor de tabla */\r
 :host {\r
   display: block;\r
   user-select: none;\r
@@ -68,8 +68,6 @@ a {\r
 }\r
 \r
 .table-container {\r
-  max-height: 561px;\r
-  /* Altura m\xE1xima para el \xE1rea desplazable */\r
   overflow-y: auto;\r
   /* Activar desplazamiento vertical */\r
   border: 1px solid #ccc;\r
@@ -85,7 +83,7 @@ a {\r
 }\r
 `;(function(){let f=document.createElement("template");f.innerHTML=`
 		    <style>
-          ${E}
+          ${x}
         </style>
         <div class="table-container">
             <table>
@@ -93,4 +91,4 @@ a {\r
                 <tbody></tbody>
             </table>
         </div>
-	`;class C extends HTMLElement{constructor(){super(),this.attachShadow({mode:"open"}).appendChild(f.content.cloneNode(!0))}onCustomWidgetBeforeUpdate(t){}onCustomWidgetAfterUpdate(t){t.selectionTable&&t.alignmentHeader&&t.alignmentData&&t.totals&&t.hyper&&this._setTableElements(t.selectionTable,t.alignmentHeader,t.alignmentData,t.totals,t.hyper)}_calculateTotals(t,r){let p=r.map(o=>{let n=t.reduce((e,i)=>e+parseInt(i[o]),0);return{key:o,value:n.toLocaleString("de-DE")}});return Object.keys(t[0]).map((o,n)=>{if(n===0){let e=document.createElement("th");return e.textContent="TOTAL:",e.classList.add("totals"),e}if(n===1){let e=document.createElement("th");return e.textContent="(Records: "+t.length+")",e.classList.add("record-count"),e}if(r.includes(o)){let e=document.createElement("th");return e.textContent=p[r.indexOf(o)].value,e.classList.add("totals"),e}else{let e=document.createElement("th");return e.textContent="",e}})}_setTableElements(t,r,p,u,y){let o=Object.keys(t[0]),n=o.map((a,m)=>{let s=document.createElement("th");s.textContent=a;let l="text-align: "+r[m]+";";return s.setAttribute("style",l),s}),e=document.createElement("tr");e.append(...n);let i=document.createElement("tr"),g=Object.values(t[0])[0]==="No results found";g===!1&&i.append(...this._calculateTotals(t,u));let h=this.shadowRoot.querySelector("thead");if(h.innerHTML="",h.append(e),h.append(i),g){let a=this.shadowRoot.querySelector("tbody");a.innerHTML="";return}let T=t.map(a=>Object.values(a)).map(a=>{let m=document.createElement("tr"),s=a.map((l,b)=>{let c=document.createElement("td");if(u.includes(o[b]))c.textContent=parseInt(l).toLocaleString("de-DE");else if(y.includes(o[b])){let d=document.createElement("a");d.textContent=l;let O="https://my334772-sso.crm.ondemand.com/sap/ap/ui/clogin?bo_ns=http://sap.com/thingTypes&bo=COD_GENERIC&node=Root&operation=OnExtInspect&param.InternalID="+l+"&param.Type=COD_OPPORTUNITY_THINGTYPE&sapbyd-agent=TAB&OBNRedirect=X";d.href=O,d.target="_blank",d.rel="noopener noreferrer",c.appendChild(d)}else c.textContent=l;let w="text-align: "+p[b]+";";return c.setAttribute("style",w),c});return m.append(...s),m}),x=this.shadowRoot.querySelector("tbody");x.innerHTML="",x.append(...T)}}customElements.define("com-ggh-table",C)})();})();
+	`;class C extends HTMLElement{constructor(){super(),this.attachShadow({mode:"open"}).appendChild(f.content.cloneNode(!0))}onCustomWidgetBeforeUpdate(e){}onCustomWidgetAfterUpdate(e){e.selectionTable&&e.configTable&&e.tableContainerHeight&&this._setTableElements(e.selectionTable,e.configTable,e.tableContainerHeight)}_calculateTotals(e,p){let s=p.filter(o=>o.total==="true").map(o=>o.column),r=s.map(o=>{let l=e.reduce((t,d)=>t+parseInt(d[o]),0);return{key:o,value:l.toLocaleString("de-DE")}});return Object.keys(e[0]).map((o,l)=>{if(l===0){let t=document.createElement("th");return t.textContent="TOTAL:",t.classList.add("totals"),t}if(l===1){let t=document.createElement("th");return t.textContent="(Records: "+e.length+")",t.classList.add("record-count"),t}if(s.includes(o)){let t=document.createElement("th");return t.textContent=r[s.indexOf(o)].value,t.classList.add("totals"),t}else{let t=document.createElement("th");return t.textContent="",t}})}_setTableElements(e,p,s){let r=Object.keys(e[0]),c=p.reduce((n,a)=>(n[a.column]=a,n),{}),g=r.map(n=>{let a=document.createElement("th");a.textContent=n;let m="text-align: "+c[n].header+";";return a.setAttribute("style",m),a}),o=document.createElement("tr");o.append(...g);let l=document.createElement("tr"),t=Object.values(e[0])[0]==="No results found";t===!1&&l.append(...this._calculateTotals(e,p));let d=this.shadowRoot.querySelector("thead");if(d.innerHTML="",d.append(o),d.append(l),t){let n=this.shadowRoot.querySelector("tbody");n.innerHTML="";return}let E=e.map(n=>Object.values(n)).map(n=>{let a=document.createElement("tr"),m=n.map((h,b)=>{let i=document.createElement("td");if(c[r[b]].total==="true")i.textContent=parseInt(h).toLocaleString("de-DE");else if(c[r[b]].hyper==="true"){let u=document.createElement("a");u.textContent=h;let O="https://my334772-sso.crm.ondemand.com/sap/ap/ui/clogin?bo_ns=http://sap.com/thingTypes&bo=COD_GENERIC&node=Root&operation=OnExtInspect&param.InternalID="+h+"&param.Type=COD_OPPORTUNITY_THINGTYPE&sapbyd-agent=TAB&OBNRedirect=X";u.href=O,u.target="_blank",u.rel="noopener noreferrer",i.appendChild(u)}else i.textContent=h;let w="text-align: "+c[r[b]].data+";";return i.setAttribute("style",w),i});return a.append(...m),a}),y=this.shadowRoot.querySelector("tbody");y.innerHTML="",y.append(...E);let T=this.shadowRoot.querySelector(".table-container");T.style.maxHeight=s}}customElements.define("com-ggh-table",C)})();})();
